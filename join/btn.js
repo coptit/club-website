1644ccad111f90b5fb79e581741f70fb7a8f8abc
naaam-h-siddhu <sudhakarsingh2004@gmail.com>
2022-08-15 13:51:26 +0530

    added response to join button in join tab

---
+++ join/btn.js
@@ -0,0 +1,13 @@
+function myFunction(){
+let btnSend = document.querySelector('button');
+let message = document.querySelector('button');
+
+btnSend.addEventListener('click', () =>{
+    btnSend.innerText = 'Joining...';
+
+    setTimeout(()=>{
+        btnSend.style.display = "Joined";
+        btnSend.innerText='Joined';
+    },1000);
+});
+}
